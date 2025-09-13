ab8d76717fe9dabd5454682d23334a882206d8ed
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-09-13 14:01:40 -0500

    started code for location choices

---
+++ scripts/LocationChoices.js
@@ -0,0 +1,12 @@
+export const LocationChoices = () => {
+    // TODO: Fetch locations from API
+
+    let html = `
+    <div class="survey-input">
+        <label for="location"><h2>What type of area do you live in?</h2></label>
+    `
+
+    html += `</div>`
+
+    return html
+}
