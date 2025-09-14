2a9261b686cb0367b82cb88fd6f9ddc016e90bb9
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-09-14 03:53:20 -0500

    completed the code for radio buttons to choose for location

--- scripts/LocationChoices.js
+++ scripts/LocationChoices.js
@@ -1,12 +1,22 @@
-export const LocationChoices = () => {
-    // TODO: Fetch locations from API
+export const LocationChoices = async () => {
+    // use await fetch to get the array of locations
+    const response = await fetch("http://localhost:8088/socioLocations")
+
+    // await the response and store it in a variable
+    const locations = await response.json()
 
     let html = `
-    <div class="survey-input">
+    <div class="survey-input" id="location-choice">
         <label for="location"><h2>What type of area do you live in?</h2></label>
     `
 
-    html += `</div>`
+    for (const location of locations) {
+        html += `<input type="radio" name="location" value="${location.id}"> ${location.label}`
+    }
+
+    html += `
+    </div>
+    `
 
     return html
 }
