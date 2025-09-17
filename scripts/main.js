567c9dec55d35b73d8eb9c998ca9f43beb2a31f2
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-09-16 20:39:16 -0500

    added logic to show submissions upon the webpage

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,21 @@
 import { JeanChoices } from "./JeanChoices.js"
 import { LocationChoices } from "./LocationChoices.js"
 import { SubmissionButton } from "./SubmissionButton.js"
-
-import "./eventHub.js"
+import { SubmissionList } from "./SubmissionList.js"
 
 const container = document.querySelector("#container")
 
 const render = async () => {
+    const jeansHTML = JeanChoices()
+    const locationsHTML = await LocationChoices()
+    const buttonHTML = SubmissionButton()
+    const submissionsHTML = await SubmissionList()
+
     container.innerHTML = `
-            ${JeanChoices()}
-            ${await LocationChoices()}
-            ${SubmissionButton()}
+        ${jeansHTML}
+        ${locationsHTML}
+        ${buttonHTML}
+        ${submissionsHTML}
     `
 }
 
